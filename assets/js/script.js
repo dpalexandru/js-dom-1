a3c05f87df2d7137019083e872a18e10e7f5b455
dpalexandru <alexpoian91@hotmail.com>
2025-07-09 15:33:30 +0200

    feat: select element of dom

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// definisco gli elementi da utilizzare 
+const foto = document.getElementById('lampadina');
+const button = document.getElementById('accendiSpegni');
+
+
+console.log(foto.src);
+console.log(button);
+
+
+
+
+
